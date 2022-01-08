c2312397408b1592c22fe194fd0a237f63d90c64
J. David <jbermeo10@gmail.com>
2022-01-07 22:43:54 -0500

    actualizando proyecto con img width and height

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ id, name, email }) => {
   return (
     //<h1>Robo Friends</h1>
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} width={200} height={200}/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
