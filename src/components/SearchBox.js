20459887362680dadf954787f1cc4815c9239c65
J. David <jbermeo10@gmail.com>
2022-01-07 19:29:50 -0500

    actualizando proyecto con favicon

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchChange}) => {
   return (
     <div className='pa2'>
       <input
+        aria-label='busque sus robots'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='busque sus robots'
